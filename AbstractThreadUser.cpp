b562f5bb9ab7e4bf6b83af489c230c3be48f6f6d
VeraKuzmina <vera-flashka@mail.ru>
2019-12-16 19:32:35 +0300

    initial project 2

---
+++ AbstractThreadUser.cpp
@@ -0,0 +1,62 @@
+#include "AbstractThreadUser.h"
+#include <fstream>
+#include <mutex>
+
+AbstractThreadUser::AbstractThreadUser(int n, int m, const char* a, const char* b, const char* d)
+{
+	this->n = n;
+	this->m = m;
+	A = new int[n];
+	B = new int[m];
+	D = new int[m + n];
+	for (int i = 0; i < n; i++)
+		A[i] = NULL;
+	for (int i = 0; i < m; i++)
+		B[i] = NULL;
+	for (int i = 0; i < m+n; i++)
+		D[i] = NULL;
+	af = a;
+	bf = b;
+	df = d;
+}
+
+AbstractThreadUser::~AbstractThreadUser() 
+{
+	delete[] A;
+	delete[] B;
+	delete[] D;
+}
+
+void AbstractThreadUser::creationArray(int &i, int* Arr)
+{
+	int aNext;
+	bool fl = true;
+	while (fl) {
+		aNext = rand() % 11 + 1;
+		fl = false;
+		for (int j = 0; j < i; j++)
+		if (Arr[j] == aNext)
+			fl = true;
+	}
+	Arr[i] = aNext;
+}
+
+int AbstractThreadUser::arrayD(int i, int* A, int* B, int* D, int n, int &k)
+{
+	if (A[i] != NULL && i != n) {
+		bool fl = false;
+		int r = 0;
+		while (B[r] && r<n) {
+			if (B[r] == A[i])
+				fl = true;
+			r++;
+		}
+		for (int l = 0; l < k; l++)
+			if (D[l] == A[i])
+				fl = false;
+		if (fl)
+			D[k++] = A[i];
+		i++;
+	}
+	return i;
+}
---
+++ AbstractThreadUser.h
@@ -0,0 +1,26 @@
+#pragma once
+#define NULL 0
+#include <fstream>
+#include <mutex>
+
+class AbstractThreadUser
+{
+public:
+	AbstractThreadUser(int n, int m, const char* a, const char* b, const char* d);
+	~AbstractThreadUser();
+	virtual void FirstThread() = 0;
+	virtual void SecondThread() = 0;
+	virtual void MainProg() = 0;
+	void static creationArray(int &i, int* A);
+	int static arrayD(int i, int* A, int* B, int* D, int n, int &k);
+protected:
+	int n;
+	int m;
+	int *A;
+	int *B;
+	int *D;
+	const char *af;
+	const char *bf;
+	const char *df;
+};
+
